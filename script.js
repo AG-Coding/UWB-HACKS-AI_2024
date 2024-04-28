d96a7448415e55dbdd2e42c317ce9182d53ca531
Anish <56240492+AG-Coding@users.noreply.github.com>
2024-04-27 23:02:09 -0700

    huh

--- script.js
+++ script.js
@@ -24,19 +24,23 @@ recognition.onend = function() {
 
 const expectedTranscriptLength = 100;
 
-let recognition = new webkitSpeechRecognition();
-recognition.continuous = true;
-recognition.interimResults = true;
+let recognition;
 
-recognition.onresult = function(event) {
-  let interimTranscript = '';
-  for (let i = event.resultIndex; i < event.results.length; ++i) {
-    if (event.results[i].isFinal) {
-      analyzeSpeech(event.results[i][0].transcript);
-    } else {
-      interimTranscript += event.results[i][0].transcript;
+window.onload = function() {
+  recognition = new webkitSpeechRecognition();
+  recognition.continuous = true;
+  recognition.interimResults = true;
+
+  recognition.onresult = function(event) {
+    let interimTranscript = '';
+    for (let i = event.resultIndex; i < event.results.length; ++i) {
+      if (event.results[i].isFinal) {
+        analyzeSpeech(event.results[i][0].transcript);
+      } else {
+        interimTranscript += event.results[i][0].transcript;
+      }
     }
-  }
+  };
 };
 
 function analyzeSpeech(transcript) {
